02a17e59d82d15cfb3180205c502cabebb85f71f
LázaroK <lazarolk23@hotmail.com>
2025-04-23 13:12:01 -0300

    feat: add AuditorAware implementation to retrieve auditor

---
+++ src/main/java/com/example/demo/audit/AuditorAwareImp.java
@@ -0,0 +1,24 @@
+package com.example.demo.audit;
+
+import com.example.demo.entities.User;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@Configuration
+public class AuditorAwareImp implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if(!(auth.isAuthenticated()) || auth.getPrincipal().equals("anonymousUser")){
+            return Optional.empty();
+        }
+        return Optional.of(auth.getName());
+    }
+}
