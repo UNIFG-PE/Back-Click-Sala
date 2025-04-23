7f57fd49397bea5a63056d0ca2e2c8c878e8bdbd
LázaroK <lazarolk23@hotmail.com>
2025-04-23 13:58:34 -0300

    fix: rename User table to 'users' to fix PostgreSQL reserved keyword error

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User extends AuditModel {
 
     @Column(nullable = false, length = 150)
