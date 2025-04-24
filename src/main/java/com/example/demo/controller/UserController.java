c25049d577411da41c7343c61aa46b562f935b12
LázaroK <lazarolk23@hotmail.com>
2025-04-23 22:13:42 -0300

    fix: Add @RequestMapping anotation
    Sorry guys, i rushed it..

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "users")
+@RestController
+@RequestMapping("api/users")
 public class UserController {
 
     @GetMapping
