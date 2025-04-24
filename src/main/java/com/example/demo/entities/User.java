fc2a54872aad45b9c95476a3a240805485e2a7d9
LázaroK <lazarolk23@hotmail.com>
2025-04-23 21:33:14 -0300

    fix: Rename User table

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
