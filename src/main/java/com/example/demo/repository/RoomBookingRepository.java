a71fa9a6e3a0a2ccfe6a0f32711789b2ba297d94
LázaroK <lazarolk23@hotmail.com>
2025-04-30 20:03:26 -0300

    fix: correct parameter in RoomBookingRepository and fix RequestMapping in RoomBookingController

--- src/main/java/com/example/demo/repository/RoomBookingRepository.java
+++ src/main/java/com/example/demo/repository/RoomBookingRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
+import com.example.demo.entities.RoomBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomBookingRepository extends JpaRepository<RoomBookingRepository, Long> {
+public interface RoomBookingRepository extends JpaRepository<RoomBooking, Long> {
 }
