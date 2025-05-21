7397e441e82e2c54db126cdd817389df4e02bae8
Bruno <brunoserpa15@gmail.com>
2025-05-20 22:03:01 -0300

    feat: campus endpoints

--- src/main/java/com/example/demo/controller/CampusController.java
+++ src/main/java/com/example/demo/controller/CampusController.java
@@ -1,10 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.entities.Campus;
 import com.example.demo.services.CampusService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/campus")
@@ -14,7 +15,27 @@ public class CampusController {
     private final CampusService campusService;
 
     @GetMapping("/status")
-    public String status(){
+    public String status() {
         return "Campus controller running";
+    }
+
+    @GetMapping
+    public List<Campus> findAll() {
+        return campusService.findAll();
+    }
+
+    @PostMapping
+    public Campus createCampus(@RequestBody Campus campus) {
+        return campusService.createCampus(campus);
+    }
+
+    @PutMapping("/{id}")
+    public Campus updateCampus(@PathVariable Long id, @RequestBody Campus campus) {
+        return campusService.updateCampus(id, campus);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCampus(@PathVariable Long id) {
+        campusService.delete(id);
     }
 }
--- src/main/java/com/example/demo/services/CampusService.java
+++ src/main/java/com/example/demo/services/CampusService.java
@@ -1,8 +1,11 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Campus;
 import com.example.demo.repository.CampusRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +13,25 @@ public class CampusService {
 
     private final CampusRepository campusRepository;
 
+    public List<Campus> findAll() {
+        return campusRepository.findAll();
+    }
+
+    public Campus createCampus (Campus campus) {
+        return campusRepository.save(campus);
+    }
+
+    public Campus updateCampus (Long id, Campus campus) {
+        Campus existingCampus = campusRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Campus not found"));
+
+        existingCampus.setName(campus.getName());
+        existingCampus.setAddress(campus.getAddress());
+
+        return campusRepository.save(existingCampus);
+    }
+
+    public void delete (Long id) {
+        campusRepository.deleteById(id);
+    }
 }
