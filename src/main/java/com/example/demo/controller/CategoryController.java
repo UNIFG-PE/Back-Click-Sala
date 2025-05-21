81457f5a33baf8ef75998016f3e192c647d6bc1f
Bruno <brunoserpa15@gmail.com>
2025-05-20 22:01:21 -0300

    feat: category endpoints

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.entities.Campus;
+import com.example.demo.entities.Category;
 import com.example.demo.services.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/categories")
@@ -18,4 +20,23 @@ public class CategoryController {
         return "Category controller running";
     }
 
+    @GetMapping
+    public List<Category> findAll() {
+        return categoryService.findAll();
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.createCategory(category);
+    }
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        return categoryService.updateCategory(id, category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.delete(id);
+    }
 }
--- src/main/java/com/example/demo/services/CategoryService.java
+++ src/main/java/com/example/demo/services/CategoryService.java
@@ -1,8 +1,11 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Category;
 import com.example.demo.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +13,25 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Long id, Category updatedCategory) {
+        Category existingCategory = categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        existingCategory.setName(updatedCategory.getName());
+        existingCategory.setDescription(updatedCategory.getDescription());
+
+        return categoryRepository.save(existingCategory);
+    }
+
+    public void delete (Long id) {
+        categoryRepository.deleteById(id);
+    }
 }
