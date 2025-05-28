616503ac417bbe32f18cfda4539799358faa447a
julio573 <julio8497@hotmail.com>
2025-05-28 14:13:24 -0300

    Feat: add DTO request data

---
+++ src/main/java/com/example/demo/dto/UserRequestDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequestDTO {
+
+    private String fullName;
+    private String cpf;
+    private String phoneNumber;
+    private String password;
+    private String email;
+}
+
