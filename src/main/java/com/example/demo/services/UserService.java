c2e2bb99ec7f39b617b95456875a23e37e8f3c5b
julio573 <julio8497@hotmail.com>
2025-05-28 14:08:23 -0300

    Feat: add user register function

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,6 +1,8 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.User;
 import com.example.demo.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +11,10 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
 
 }
