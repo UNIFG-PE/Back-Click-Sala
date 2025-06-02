d9811c11accb3add71bd56f5386bbdf8f537586f
Bruno <brunoserpa15@gmail.com>
2025-06-02 13:05:01 -0300

    feat: add GET /campus endpoint

--- src/main/java/com/example/demo/controller/CampusController.java
+++ src/main/java/com/example/demo/controller/CampusController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.CampusResponseDTO;
 import com.example.demo.services.CampusService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/campus")
@@ -13,8 +17,9 @@ public class CampusController {
 
     private final CampusService campusService;
 
-    @GetMapping("/status")
-    public String status(){
-        return "Campus controller running";
+    @GetMapping
+    public ResponseEntity<List<CampusResponseDTO>> getAll() {
+        List<CampusResponseDTO> campus = campusService.findAll();
+        return ResponseEntity.ok(campus);
     }
 }
--- src/main/java/com/example/demo/services/CampusService.java
+++ src/main/java/com/example/demo/services/CampusService.java
@@ -1,8 +1,11 @@
 package com.example.demo.services;
 
 import com.example.demo.repository.CampusRepository;
+import com.example.demo.dto.CampusResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +13,9 @@ public class CampusService {
 
     private final CampusRepository campusRepository;
 
+    public List<CampusResponseDTO> findAll() {
+        return campusRepository.findAll().stream()
+                .map(CampusResponseDTO::new)
+                .toList();
+    }
 }
