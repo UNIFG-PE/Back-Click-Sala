b4502d3bd4ed19fa31b3ae522f11df0526c7c81a
julio573 <julio8497@hotmail.com>
2025-06-02 14:15:22 -0300

    fix: DTO class name fixed

--- src/main/java/com/example/demo/dto/UserRegisterResponseDTO.java
+++ src/main/java/com/example/demo/dto/UserRegisterResponseDTO.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserResponseDTO {
+public class UserRegisterResponseDTO {
     private String fullName;
     private String email;
 }
