5521ccc5be0dd91e4469651e76fbecf5f716b28a
julio573 <julio8497@hotmail.com>
2025-06-02 14:39:18 -0300

    feat: add dto to function

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,5 +1,9 @@
 package com.example.demo.services;
 
+import com.example.demo.dto.UserRequestDTO;
+import com.example.demo.dto.UserResponseDTO;
+import com.example.demo.dto.mapper.UserMapper;
+import com.example.demo.entities.User;
 import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +13,10 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserResponseDTO createUser(UserRequestDTO userRequestDTO) {
+        return userMapper.toResponseDTO(userRepository.save(userMapper.toEntity(userRequestDTO)));
+    }
 
 }
