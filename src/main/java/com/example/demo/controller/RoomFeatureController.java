4a50ab893e79195a6dc45fac199612b1e96427b4
Bruno <brunoserpa15@gmail.com>
2025-06-12 12:55:08 -0300

    feat: add GET /roomfeatures endpoint

--- src/main/java/com/example/demo/controller/RoomFeatureController.java
+++ src/main/java/com/example/demo/controller/RoomFeatureController.java
@@ -1,10 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.RoomFeatureResponseDTO;
 import com.example.demo.services.RoomFeatureService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/roomfeatures")
@@ -16,5 +21,17 @@ public class RoomFeatureController {
     @GetMapping("/status")
     public String status (){
         return "RoomFeature controller running";
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoomFeatureResponseDTO>> getAll() {
+        List<RoomFeatureResponseDTO> feature = roomFeatureService.findAll();
+        return ResponseEntity.ok(feature);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoomFeatureResponseDTO> getById(@PathVariable Long id) {
+        RoomFeatureResponseDTO feature = roomFeatureService.getFeatureById(id);
+        return ResponseEntity.ok(feature);
     }
 }
--- src/main/java/com/example/demo/services/RoomFeatureService.java
+++ src/main/java/com/example/demo/services/RoomFeatureService.java
@@ -1,8 +1,13 @@
 package com.example.demo.services;
 
+import com.example.demo.dto.RoomFeatureResponseDTO;
+import com.example.demo.entities.RoomFeature;
 import com.example.demo.repository.RoomFeatureRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +15,16 @@ public class RoomFeatureService {
 
     private final RoomFeatureRepository roomFeatureRepository;
 
+    public List<RoomFeatureResponseDTO> findAll() {
+        return roomFeatureRepository.findAll().stream()
+                .map(RoomFeatureResponseDTO::new)
+                .toList();
+    }
+
+    public RoomFeatureResponseDTO getFeatureById(Long id) {
+        RoomFeature feature = roomFeatureRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Feature not found"));
+
+        return new RoomFeatureResponseDTO(feature);
+    }
 }
