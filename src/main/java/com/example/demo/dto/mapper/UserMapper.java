4ce2c1eb26fe09b8ea18b706d78379b892c57d3c
julio573 <julio8497@hotmail.com>
2025-06-16 12:44:28 -0300

    Feat: DTO request updated

--- src/main/java/com/example/demo/dto/mapper/UserMapper.java
+++ src/main/java/com/example/demo/dto/mapper/UserMapper.java
@@ -4,9 +4,16 @@ import com.example.demo.dto.UserRegisterRequestDTO;
 import com.example.demo.dto.UserRegisterResponseDTO;
 import com.example.demo.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    @Mapping(target = "status", constant = "ACTIVE")
+    @Mapping(target = "roles", ignore = true)
+    @Mapping(target = "supportTicket", ignore = true)
+    @Mapping(target = "photoUrl", ignore = true)
     User toEntity(UserRegisterRequestDTO dto);
+
     UserRegisterResponseDTO toResponseDTO(User user);
 }
