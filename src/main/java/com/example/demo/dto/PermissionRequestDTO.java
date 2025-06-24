a2258b3778394ff69948206449fc3e95f84b3f48
Vinícius Rosa <viniciusrosalima14@gmail.com>
2025-06-24 18:26:27 -0300

    feat: adding permissions's DTO

---
+++ src/main/java/com/example/demo/dto/PermissionRequestDTO.java
@@ -0,0 +1,7 @@
+package com.example.demo.dto;
+
+public record PermissionRequestDTO(
+    String name,
+    String description
+) {
+}
---
+++ src/main/java/com/example/demo/dto/PermissionResponseDTO.java
@@ -0,0 +1,8 @@
+package com.example.demo.dto;
+
+public record PermissionResponseDTO(
+    Long id,
+    String name,
+    String description
+) {
+}
